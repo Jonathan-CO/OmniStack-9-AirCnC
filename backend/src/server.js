c4da5aab9932d1b95781f57b142322749c032ac5
Jonathan-CO <j_cunha_oliveira@hotmail.com>
2019-10-02 09:41:31 -0300

    API backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,22 @@
 const express  = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
 
 const app=express();
 
-app.get('/', (req, res) =>{
+mongoose.connect('mongodb+srv://omnistack-9:omnistack-9@omnistack-9-sgqrz.mongodb.net/semana09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
     //req representa a requisição (dados da req)
     //res representa a resposta (devolução à req)
+    //req.query = acessar params (filtros)
+    //req.params = acessar route params (edição, delete)
+    //req.body = acessar corpo da requisição (criação e edição)
 
-    return res.json({ message: "Iniciando a Vida React"});
 
-});
+app.use(express.json());
+app.use(routes);
+
+
 app.listen(3333);
