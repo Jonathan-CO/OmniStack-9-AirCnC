5ed5cdad4ff99e9178cbeeba557db845a8555af2
Jonathan-CO <j_cunha_oliveira@hotmail.com>
2019-10-04 21:47:28 -0300

    finalizando frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'; // bib global para interfaces 
+import ReactDOM from 'react-dom'; // bib para lidar com a árvore html
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
